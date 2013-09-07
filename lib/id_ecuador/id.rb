7b3df895cd228528d9c9147ee2e68b666fa2fb78
Mario Andrés Correa <admin@macool.me>
2013-09-07 18:07:33 -0500

    tests passing.

--- lib/id_ecuador/id.rb
+++ lib/id_ecuador/id.rb
@@ -22,15 +22,13 @@ module IdEcuador
     end
     def validate!
       @already_validated = true
-      validate_length and evaluate_province_code and evaluate_third_digit and 
+      validate_length and evaluate_province_code and evaluate_third_digit
     end
-    
-  protected
     def already_validated
       !!@already_validated
     end
     
-  private
+  protected
     def validate_length
       if [10, 13].include?(@id.length)
         true
@@ -52,17 +50,94 @@ module IdEcuador
     end
     def evaluate_third_digit
       @third_digit = @id[2].to_i
+
+      # to keep products in next method:
+      @products = []
+
       if [7, 8].include?(@third_digit)
         @errors << "Tercer dígito es inválido"
         return false
       elsif @third_digit < 6
         @tipo_id = "Persona natural"
+        validate_last_digits_for_persona_natural and evaluate_sums_for_persona_natural!
       elsif @third_digit == 6
         @tipo_id = "Sociedad pública"
+        validate_last_digits_for_sociedad_publica and evaluate_sums_for_sociedad_publica!
       elsif @third_digit == 9
         @tipo_id = "Sociedad privada o extranjera"
+        validate_last_digits_for_sociedad_privada and evaluate_sums_for_sociedad_privada!
       end
       return true
+    end
+    def validate_last_digits_for_persona_natural
+      if @id.length > 10 and @id.slice(10,3) != "001"
+        @errors << "RUC de persona natural debe terminar en 001"
+        false
+      else
+        true
+      end
+    end
+    def validate_last_digits_for_sociedad_publica
+      if @id.slice(9,4) != "0001"
+        @errors << "RUC de empresa de sector público debe terminar en 0001"
+        false
+      else
+        true
+      end
+    end
+    def validate_last_digits_for_sociedad_privada
+      if @id.slice(10,3) != "001"
+        @errors << "RUC de entidad privada o extranjera debe terminar en 001"
+        false
+      else
+        true
+      end
+    end
+    def evaluate_sums_for_persona_natural!
+      @modulus = 10
+      @verifier = @id.slice(9,1).to_i
+      p = 2
+      @id.slice(0,9).chars do |c|
+        product = c.to_i * p
+        if product >= 10 then product -= 9 end
+        @products << product
+        p = if p == 2 then 1 else 2 end
+      end
+      do_sums!
+      self
+    end
+    def evaluate_sums_for_sociedad_publica!
+      @modulus = 11
+      @verifier = @id.slice(8,1).to_i
+      multipliers = [3, 2, 7, 6, 5, 4, 3, 2]
+      (0..7).each do |i|
+        @products[i] = @id[i].to_i * multipliers[i]
+      end
+      @products[8] = 0
+      do_sums!
+      self
+    end
+    def evaluate_sums_for_sociedad_privada!
+      @modulus = 11
+      @verifier = @id.slice(9,1).to_i
+      multipliers = [4, 3, 2, 7, 6, 5, 4, 3, 2]
+      (0..8).each do |i|
+        @products[i] = @id[i].to_i * multipliers[i]
+      end
+      do_sums!
+      self
+    end
+    def do_sums!
+      sum = 0
+      @products.each do |product|
+        sum += product
+      end
+      res = sum % @modulus
+      digit = if res == 0 then 0 else @modulus - res end
+      unless digit == @verifier
+        @errors << "ID inválida"
+      end
+      self
     end
   end
 end
