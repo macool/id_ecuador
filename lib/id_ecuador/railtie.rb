001adf0c70e0e82215d89e7eb28d593fde709956
Mario Andrés Correa <admin@macool.me>
2013-09-08 14:13:23 -0500

    writing docs [ci skip]

--- lib/id_ecuador/railtie.rb
+++ lib/id_ecuador/railtie.rb
@@ -1,4 +1,6 @@
 module IdEcuador
+  # extend <tt>IdEcuador</tt> module into Rails' <tt>ActiveRecord::Base</tt>
+  # Permite llamar a <tt>validates_id</tt> dentro de un modelo de Rails
   class Railtie < Rails::Railtie
     intializer 'id_ecuador.model_additions' do
       ActiveSupport.on_load :active_record do
