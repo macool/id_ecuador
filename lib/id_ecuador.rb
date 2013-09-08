1c4f13addab4773b12fd3a3835a0094741430e46
Mario Andrés Correa <admin@macool.me>
2013-09-08 14:10:11 -0500

    writing documentation [ci skip]

--- lib/id_ecuador.rb
+++ lib/id_ecuador.rb
@@ -3,6 +3,17 @@ require "id_ecuador/id"
 require "id_ecuador/model_additions"
 require "id_ecuador/railtie" if defined? Rails
 
+# Este módulo debe servir como <em>namespace</em>
+#
+# @author macool <a@macool.me>
+#
+# Contiene:
+#   Clases:
+#   * <tt>Id</tt>
+#
+#   Módulos:
+#   * <tt>ModelAdditions</tt>
+#   * <tt>Railtie</tt>
 module IdEcuador
   # Your code goes here...
 end
