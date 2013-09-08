a71359091855f5cc27178c75a3a9d6fcd6d424d8
Mario Andrés Correa <admin@macool.me>
2013-09-07 19:22:54 -0500

    enhancing things for Rails

--- lib/id_ecuador/model_additions.rb
+++ lib/id_ecuador/model_additions.rb
@@ -1,14 +1,38 @@
 module IdEcuador
   module ModelAdditions
     def validates_id(attribute)
+
+      # poner métodos:
+
+      # getter del atributo que siempre retorna una instancia de IdEcuador::Id y se encarga de hacer cache basado en el valor del atributo
+      class_eval <<-EVAL
+        def #{attribute}_id_validator
+          if not @id_ecuador_validator or (send(:#{attribute}) != @id_ecuador_validator_last_id)
+            @id_ecuador_validator = IdEcuador::Id.new(send(:#{attribute}))
+            @id_ecuador_validator_last_id = send(:#{attribute})
+          end
+          @id_ecuador_validator
+        end
+      EVAL
+
+      # extensiones para .tipo_id y .codigo_provincia
+      class_eval <<-EVAL
+        def #{attribute}_tipo_id
+          #{attribute}_id_validator.tipo_id
+        end
+        def #{attribute}_codigo_provincia
+          #{attribute}_id_validator.codigo_provincia
+        end
+      EVAL
+
       validate do
-        id = IdEcuador::Id.new(send(attribute))
-        unless id.valid?
-          id.errors.each do |error|
+        unless send(:"#{attribute}_id_validator").valid?
+          send(:"#{attribute}_id_validator").errors.each do |error|
             errors.add attribute.to_sym, error
           end
         end
       end
+
     end
   end
 end
--- spec/id_ecuador/model_additions_spec.rb
+++ spec/id_ecuador/model_additions_spec.rb
@@ -4,28 +4,54 @@ require "spec_helper"
 # tests with Rails' models
 
 describe IdEcuador::ModelAdditions do
-  
-  describe "tests con id" do
     
-    class User < SuperModel::Base
-      extend IdEcuador::ModelAdditions
-      validates_id :identificacion
-    end
+  class User < SuperModel::Base
+    extend IdEcuador::ModelAdditions
+    validates_id :identificacion
+  end
 
-    describe "valida el ID de un usuario antes de guardarlo a la base de datos" do
-      it "debería decir que es válido" do
-        User.new(identificacion: "1104680135").valid?.should be_true
-      end
-      it "debería decir que es inválido" do
-        user = User.new(identificacion: "1104680134")
+  describe "valida el ID de un usuario antes de guardarlo a la base de datos" do
+    it "debería decir que es válido" do
+      User.new(identificacion: "1104680135").valid?.should be_true
+    end
+    it "debería decir que es inválido" do
+      user = User.new(identificacion: "1104680134")
+      user.save.should be_false
+      user.errors[:identificacion].should include("ID inválida")
+    end
+    it "debería decir que la longitud es inválida" do
+      user = User.new(identificacion: "12345")
+      user.save.should be_false
+      user.errors[:identificacion].should include("Longitud incorrecta")
+    end
+    describe "expirar el cache de la variable @id_ecuador_validator_last_id" do
+      it "debería decir que la cédula es válida y, al cambiarla, decir que es inválida" do
+        user = User.new
+        user.identificacion = "1104680135"
+        user.valid?.should be_true
+        user.identificacion = "1104680134"
         user.valid?.should be_false
-        user.errors[:identificacion].should include("ID inválida")
       end
-      it "debería decir que la longitud es inválida" do
-        user = User.new(identificacion: "12345")
+      it "debería decir que la cédula es inválida y, al cambiarla, decir que es válida" do
+        user = User.new
+        user.identificacion = "1104680134"
         user.valid?.should be_false
-        user.errors[:identificacion].should include("Longitud incorrecta")
+        user.identificacion = "1104680135"
+        user.valid?.should be_true
       end
+    end
+  end
+
+  describe "debe agregar métodos al atributo" do
+    it "debe agregar método `identificacion_tipo_id`" do
+      user = User.new(identificacion: "1104680135")
+      user.respond_to?(:identificacion_tipo_id).should be_true
+      user.identificacion_tipo_id.should eq("Cédula Persona natural")
+    end
+    it "debe agregar método `identificacion_codigo_provincia`" do
+      user = User.new(identificacion: "1104680135")
+      user.respond_to?(:identificacion_codigo_provincia).should be_true
+      user.identificacion_codigo_provincia.should eq(11)
     end
   end
 
