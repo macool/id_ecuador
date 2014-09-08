a9a4475092843bc1f414eb92890b0b01564d3bcb
Jose Añasco <joseanasco1@gmail.com>
2014-09-07 22:27:15 -0500

    fix deprecation rspec errors

--- spec/id_ecuador_spec.rb
+++ spec/id_ecuador_spec.rb
@@ -4,7 +4,7 @@ require "spec_helper"
 describe IdEcuador do
   it "should alias IdEcuador#new to IdEcuador::Id#new" do
     id = IdEcuador.new CEDULA_VALIDA
-    id.class.should eq(IdEcuador::Id)
-    id.id.should eq(CEDULA_VALIDA)
+    expect(id.class).to eq(IdEcuador::Id)
+    expect(id.id).to eq(CEDULA_VALIDA)
   end
 end
