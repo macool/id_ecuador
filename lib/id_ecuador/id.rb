f8b9f266f1a6401dffa8b7c58679fb8525b01714
Jose Añasco <joseanasco1@gmail.com>
2014-09-15 17:24:47 -0500

    Id class refactor

--- lib/id_ecuador/id.rb
+++ lib/id_ecuador/id.rb
@@ -19,15 +19,15 @@ module IdEcuador
   #
   # @attr_reader [Fixnum] codigo_provincia El código de la provincia del ID ingresado
   class Id
-    
+
     attr_reader :id, :errors, :tipo_id, :tipo_id_sym, :codigo_provincia
-    
+
     # @param [String] id El ID que se va a analizar
     # @param [Hash] options Las opciones
     # @option options [Boolean] :auto_validate Validar automáticamente el ID al instanciar. <tt>true</tt> por defecto
     def initialize(id="", options={})
       @id = id.to_s
-      
+
       defaults = {
         auto_validate: true
       }
@@ -53,7 +53,7 @@ module IdEcuador
     # @return [self] Método que valida
     def validate!
       @already_validated = true
-      validate_length and evaluate_province_code and evaluate_third_digit
+      validate_length && evaluate_province_code && evaluate_third_digit
       self
     end
 
@@ -61,7 +61,7 @@ module IdEcuador
     def already_validated
       !!@already_validated
     end
-    
+
   protected
     # Método llamado después de #initialize y #id=
     # Se encarga de correr la validación si options[:auto_validate] es true
@@ -70,6 +70,7 @@ module IdEcuador
       validate! if @options[:auto_validate]
       self
     end
+
     def validate_length
       if [10, 13].include?(@id.length)
         true
@@ -78,10 +79,11 @@ module IdEcuador
         false
       end
     end
+
     def evaluate_province_code
       provinces = 22
-      code = @id.slice(0,2).to_i
-      if code < 1 or code > provinces
+      code = @id[0,2].to_i
+      if code < 1 || code > provinces
         @errors << "Código de provincia incorrecto"
         false
       else
@@ -89,100 +91,115 @@ module IdEcuador
         true
       end
     end
+
     def evaluate_third_digit
       @third_digit = @id[2].to_i
 
       # to keep products in next method:
       @products = []
+      invalid_third_digit = [7, 8].include?(@third_digit)
 
-      if [7, 8].include?(@third_digit)
-        @errors << "Tercer dígito es inválido"
-        return false
-      elsif @third_digit < 6
-        if @id.length > 10
-          @tipo_id = "RUC Persona natural"
-          @tipo_id_sym = :ruc
-        else
-          @tipo_id = "Cédula Persona natural"
-          @tipo_id_sym = :cedula
-        end
-        validate_last_digits_for_persona_natural and evaluate_sums_for_persona_natural!
-      elsif @third_digit == 6
-        @tipo_id = "Sociedad pública"
-        @tipo_id_sym = :sociedad_publica
-        validate_last_digits_for_sociedad_publica and evaluate_sums_for_sociedad_publica!
-      elsif @third_digit == 9
-        @tipo_id = "Sociedad privada o extranjera"
-        @tipo_id_sym = :sociedad_privada
-        validate_last_digits_for_sociedad_privada and evaluate_sums_for_sociedad_privada!
-      end
-      return true
+      errors << "Tercer dígito es inválido" if invalid_third_digit
+      set_as_persona_natural if @third_digit < 6
+      set_as_sociedad_publica if @third_digit == 6
+      set_as_sociedad_privada if @third_digit == 9
+
+      return !invalid_third_digit
     end
+
+    def set_as_persona_natural
+      if @id.length > 10
+        @tipo_id = "RUC Persona natural"
+        @tipo_id_sym = :ruc
+      else
+        @tipo_id = "Cédula Persona natural"
+        @tipo_id_sym = :cedula
+      end
+      validate_last_digits_for_persona_natural &&
+        evaluate_sums_for_persona_natural!
+    end
+
+    def set_as_sociedad_publica
+      @tipo_id = "Sociedad pública"
+      @tipo_id_sym = :sociedad_publica
+      validate_last_digits_for_sociedad_publica &&
+        evaluate_sums_for_sociedad_publica!
+    end
+
+    def set_as_sociedad_privada
+      @tipo_id = "Sociedad privada o extranjera"
+      @tipo_id_sym = :sociedad_privada
+      validate_last_digits_for_sociedad_privada &&
+        evaluate_sums_for_sociedad_privada!
+    end
+
     def validate_last_digits_for_persona_natural
-      if @id.length > 10 and @id.slice(10,3) != "001"
+      if @id.length > 10 && @id[10,3] != "001"
         @errors << "RUC de persona natural debe terminar en 001"
         false
       else
         true
       end
     end
+
     def validate_last_digits_for_sociedad_publica
-      if @id.slice(9,4) != "0001"
+      if @id[9,4] != "0001"
         @errors << "RUC de empresa de sector público debe terminar en 0001"
         false
       else
         true
       end
     end
+
     def validate_last_digits_for_sociedad_privada
-      if @id.slice(10,3) != "001"
+      if @id[10,3] != "001"
         @errors << "RUC de entidad privada o extranjera debe terminar en 001"
         false
       else
         true
       end
     end
+
     def evaluate_sums_for_persona_natural!
       @modulus = 10
-      @verifier = @id.slice(9,1).to_i
+      @verifier = @id[9].to_i
       p = 2
-      @id.slice(0,9).chars do |c|
+      @id[0,9].chars do |c|
         product = c.to_i * p
-        if product >= 10 then product -= 9 end
+        product -= 9 if product >= 10
         @products << product
-        p = if p == 2 then 1 else 2 end
+        p = p == 2 ? 1 : 2
       end
       do_sums!
       self
     end
+
     def evaluate_sums_for_sociedad_publica!
-      @modulus = 11
-      @verifier = @id.slice(8,1).to_i
-      multipliers = [3, 2, 7, 6, 5, 4, 3, 2]
-      (0..7).each do |i|
-        @products[i] = @id[i].to_i * multipliers[i]
-      end
+      evaluate_sums_for_sociedad(8, [3, 2, 7, 6, 5, 4, 3, 2])
       @products[8] = 0
       do_sums!
       self
     end
+
     def evaluate_sums_for_sociedad_privada!
-      @modulus = 11
-      @verifier = @id.slice(9,1).to_i
-      multipliers = [4, 3, 2, 7, 6, 5, 4, 3, 2]
-      (0..8).each do |i|
-        @products[i] = @id[i].to_i * multipliers[i]
-      end
+      evaluate_sums_for_sociedad(9, [4, 3, 2, 7, 6, 5, 4, 3, 2])
       do_sums!
       self
     end
-    def do_sums!
-      sum = 0
-      @products.each do |product|
-        sum += product
+
+    def evaluate_sums_for_sociedad(index, multipliers)
+      @modulus = 11
+      @verifier = @id[index].to_i
+
+      (0...index).each do |i|
+        @products[i] = @id[i].to_i * multipliers[i]
       end
+    end
+
+    def do_sums!
+      sum = @products.reduce(:+)
       res = sum % @modulus
-      digit = if res == 0 then 0 else @modulus - res end
+      digit = res == 0 ? 0 : @modulus - res
       unless digit == @verifier
         @errors << "ID inválida"
       end
